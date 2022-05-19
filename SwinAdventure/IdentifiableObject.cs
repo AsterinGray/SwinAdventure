25c96a10c2adf4eeab95778f4a982a70742f1e76
AsterinGray <raymondtiandinatan@gmail.com>
2022-05-19 23:34:31 +0700

    Add project files.

---
+++ SwinAdventure/IdentifiableObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SwinAdventure
+{
+    public class IdentifiableObject
+    {
+        private List<string> _identifiers = new List<string>();
+
+        public IdentifiableObject(string[] idents)
+        {
+            _identifiers.AddRange(idents);
+        }
+
+        public bool AreYou(string id)
+        {
+            return _identifiers.Contains(id);
+        }
+
+        public string FirstId
+        {
+            get { return _identifiers[0]; }
+        }
+
+        public void AddIdentifier(string id)
+        {
+            _identifiers.Add(id.ToLower());
+        }
+    }
+}
---
+++ SwinAdventureTests/IdentifiableObjectTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SwinAdventure.Tests
+{
+    [TestClass()]
+    public class IdentifiableObjectTests
+    {
+        [TestMethod()]
+        public void TestAreYou()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "fred", "bob" });
+            Assert.IsTrue(id.AreYou("fred"));
+            Assert.IsTrue(id.AreYou("bob"));
+        }
+
+        [TestMethod()]
+        public void TestNotAreYou()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "fred", "bob" });
+            Assert.IsFalse(id.AreYou("wilma"));
+            Assert.IsFalse(id.AreYou("boby"));
+        }
+
+        [TestMethod()]
+        public void TestCaseSensitive()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "fred", "bob" });
+            Assert.IsFalse(id.AreYou("FRED"));
+            Assert.IsFalse(id.AreYou("bOB"));
+        }
+
+        [TestMethod()]
+        public void TestFirstId()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "fred", "bob" });
+            Assert.IsTrue(id.FirstId == "fred");
+        }
+
+        [TestMethod()]
+        public void TestAddId()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "fred", "bob" });
+            id.AddIdentifier("wilma");
+            Assert.IsTrue(id.AreYou("fred"));
+            Assert.IsTrue(id.AreYou("bob"));
+            Assert.IsTrue(id.AreYou("wilma"));
+        }
+    }
+}
